1a75b15e865c452e1d77a61bda765f38848057d6
Kim Vo <kimlongt.vo@gmail.com>
2020-05-21 15:13:07 -0700

    cors-anywhere added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,26 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Loader from './components/layout/Loader';
 import Alert from './components/layout/Alert';
 
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+
 export default class App extends Component {
+
+    
     constructor(props) {
         super(props);
         this.state = {
@@ -31,7 +50,7 @@ export default class App extends Component {
     }
 
     getData = text => {
-        fetch(`https://api.stocktwits.com/api/2/streams/symbol/${text}.json`,{
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.stocktwits.com/api/2/streams/symbol/${text}.json`,{
             mode: 'cors'
         })
             .then(response => {
